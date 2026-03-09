f5ab17ea0b0523b29acc563d596eed626d6bcd6c
LuYF-Lemon-love <3555028709@qq.com>
2026-03-09 23:09:30 +0800

    feat: 添加新的提供商图标

--- packages/opencode/src/index.ts
+++ packages/opencode/src/index.ts
@@ -205,9 +205,5 @@ try {
   }
   process.exitCode = 1
 } finally {
-  // Some subprocesses don't react properly to SIGTERM and similar signals.
-  // Most notably, some docker-container-based MCP servers don't handle such signals unless
-  // run using `docker run --init`.
-  // Explicitly exit to avoid any hanging subprocesses.
   process.exit()
 }
